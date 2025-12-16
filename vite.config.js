71379baf417bdf385862d05ae5420f572a2586b5
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2025-12-16 16:13:20 -0300

    att routes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/my-dashboard/' // <-- importante para o GitHub Pages
+  base: '/my-dashboard/' 
 })
